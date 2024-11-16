0b0ed52e9ba843dabe7bb2e2680aa4e6331c9805
Kirill Karpov <rin.veber@gmail.com>
2024-11-16 21:56:33 +0400

    deploy2

--- src/services/entities/authApi.ts
+++ src/services/entities/authApi.ts
@@ -32,6 +32,7 @@ export const authApi = createApi({
         body: JSON.stringify({ email, password }),
       }),
       transformResponse: (response: RegisterResponse) => {
+        console.log(response)
         if (response.success) {
           setCookie("accessToken", response.accessToken);
           setCookie("refreshToken", response.refreshToken);
