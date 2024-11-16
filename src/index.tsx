3aae5c6dd7236e19fa482874681a4f94d1506afb
Kirill Karpov <rin.veber@gmail.com>
2024-11-16 22:46:02 +0400

    hash router for gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,33 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
-import ErrorBoundary from "./utils/error-boundary/error-boundary";
-import { Provider } from "react-redux";
-import store from "./services/store";
-import AppRouter from "./router/AppRouter";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, HashRouter} from 'react-router-dom';
+import ErrorBoundary from './utils/error-boundary/error-boundary';
+import {Provider} from 'react-redux';
+import store from './services/store';
+import AppRouter from './router/AppRouter';
+
+const isLocalhost = ['127.0.0.1', 'localhost'].includes(
+  window.location.hostname,
+);
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement,
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
     <ErrorBoundary>
       <Provider store={store}>
-        <BrowserRouter>
-          <AppRouter />
-        </BrowserRouter>
+        {isLocalhost ? (
+          <BrowserRouter>
+            <AppRouter />
+          </BrowserRouter>
+        ) : (
+          <HashRouter>
+            <AppRouter />
+          </HashRouter>
+        )}
       </Provider>
     </ErrorBoundary>
   </React.StrictMode>,
